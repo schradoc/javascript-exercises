7cbb6e36fb612776a2446af309d53798269ccbec
Christopher Schrader <schradoc@gmail.com>
2022-08-01 10:11:18 +0200

    Complete calc exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,14 @@
-const ftoc = function() {
-
+// function ftoc to convert fahrenheit to celsius rounded to one decimal point
+const ftoc = function (fahrenheit) {
+  return Math.round((((fahrenheit - 32) * 5) / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  return Math.round((celsius * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let factor = 1;
+  for (let i = 1; i <= a; i++) {
+    factor *= i;
+  }
+  return factor;
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
